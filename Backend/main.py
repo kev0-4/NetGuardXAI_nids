a147a79e6ddfd9bed8b6059549e02cba1f6ba131
Kevin Tandon <kevin.tandon.btech2022@sitpune.edu.in>
2025-04-21 19:34:36 +0530

    changed port

--- Backend/main.py
+++ Backend/main.py
@@ -211,4 +211,4 @@ def predict():
 if __name__ == "__main__":
     # Ensure plot directory exists
     os.makedirs(PLOT_DIR, exist_ok=True)
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    app.run(debug=True, host='0.0.0.0', port=8080)
